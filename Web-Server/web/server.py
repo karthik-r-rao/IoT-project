efd8487152b05e462776766fee188c98787bea45
Karthik Rao <karthikrrao30@gmail.com>
2022-01-29 00:43:49 +0530

    simple server code

--- Web-Server/web/server.py
+++ Web-Server/web/server.py
@@ -1,9 +1,12 @@
 import flask
+import flask_socketio as fsio
 
 app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = fsio.SocketIO(app, always_connect=True)
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def home():
     return flask.render_template("home.html")
 
@@ -11,9 +14,12 @@ def home():
 def team():
     return flask.render_template("team.html")
 
-@app.route("/hello", methods=['GET', 'POST'])
-def hello():
-    return flask.render_template("hello.html")
+@socketio.on('my message')
+def my_custom_event(message):
+    print(f"[SERVER] Received message: {message}")
+    reply = "Thanks for connecting!"
+    fsio.emit('my response', reply)
+    print(f"[SERVER] Sent message: {reply}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, host="192.168.1.35", port=5000, debug=True)
